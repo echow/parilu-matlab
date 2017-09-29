f678010f0ec35727e6cdd394ec1f618076e71bde
Edmond Chow <edmond@apiary>
2017-09-29 17:02:16 -0400

    .

--- parilu.m
+++ parilu.m
@@ -1,26 +1,16 @@
-function [l u] = parilu(a, l, u, numsweeps, numthreads)
-% [l u] = parilu(a, l, u, numsweeps, numthreads)
-%  Input l and u are initial guesses.
-%  Sparsity pattern of l and u are preserved on output.
-%  
-% If you want l and u to contain structural zero elements,
-% then you must put in small values.
+function [l u] = parilu(a, l, u, sync_notsync, numsweeps, numthreads)
+% [l u] = parilu(a, l, u, sync_notsync, numsweeps, numthreads)
 
-% following code is very slow
-%
-% [Si Sj] = find(spones(l)+spones(u));
-% m = length(Si);
-% Sa = zeros(m,1);
-% for k=1:m
-%   Sa(k) = a(Si(k),Sj(k));
-% end
+pat = spones(spones(l) + spones(u));
+[Si Sj Sa] = find(a.*pat + eps*pat);
 
-pat = spones(l-diag(diag(l))+u);
-[Si Sj Sa] = find(a.*pat + 1e-99*pat);
-
-% convert indices Si and Sj to 0-based
-l = l';
+l = l'; % convert l to row-based
 [l u] = parilu_mex(int32(Si)-1, int32(Sj)-1, Sa, l, u, numsweeps, numthreads);
-l = l';
+if (sync_notsync)
+  error('synchronous version not yet implemented');
+else
+  [l u] = parilu_mex(int32(Si)-1, int32(Sj)-1, Sa, l, u, numsweeps, numthreads);
+end
+l = l'; % convert back
 
 
---
+++ parilu_ref.m
@@ -0,0 +1,61 @@
+function [l u resid] = parilu_ref(a, l, u, numsweeps)
+% [l u resid] = parilu_ref(a, l, u, numsweeps)
+%   Synchronous updates
+%
+% a         = sparse matrix should be scaled to have unit diagonal
+% l, u      = input guesses; patterns can be unrelated to matrix a
+% numsweeps = number of nonlinear fixed-point sweeps
+% resid     = nonlinear residual norm history (optional)
+
+if nargout > 2
+    resid = zeros(numsweeps+1, 1);
+    resid(1) = norm((a-l*u).*spones(a),'fro');
+end
+
+% find nonzeros of u in column-major ordering
+pat = spones(spones(l) + spones(u));
+[Si Sj Sa] = find(a.*pat + eps*pat);
+m = length(Sa);
+
+for iter=1:numsweeps
+
+    l0 = l'; % frozen l in transposed form
+    u0 = u;  % frozen u
+    
+    for k=1:m
+        i = Si(k);
+        j = Sj(k);
+        if (i>j)
+            l(i,j) = ( Sa(k) - l0(:,i)'*u0(:,j) + l0(j,i)*u0(j,j) ) / u0(j,j);
+        else
+            u(i,j) =   Sa(k) - l0(:,i)'*u0(:,j) + l0(i,i)*u0(i,j);
+            % note l(i,i) is 1
+        end
+    end
+    
+    if nargout > 2
+        resid(iter+1) = norm((a-l*u).*spones(a),'fro');
+    end
+end
+
+% below is the version if l is not transposed
+%
+% for iter=1:numsweeps
+% 
+%     l0 = l; % frozen l
+%     u0 = u; % frozen u
+% 
+%     for k=1:m
+%         i = Si(k);
+%         j = Sj(k);
+%         if (i>j)
+%             l(i,j) = Sa(k) - l0(i,:)*u0(:,j) + l0(i,j)*u0(j,j);
+%             l(i,j) = l(i,j) / u0(j,j);
+%         else
+%             u(i,j) = Sa(k) - l0(i,:)*u0(:,j) + l0(i,i)*u0(i,j);
+%         end
+%     end
+% 
+%     resid(iter+1) = norm((a-l*u).*spones(a),'fro');
+% end
+
